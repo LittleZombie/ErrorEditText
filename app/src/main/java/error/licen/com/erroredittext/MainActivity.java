12306433591c49b001dfae64fa1c6a4396541a88
P200633\Julie115 <Julie115@staff.1111.com.tw>
2017-03-16 12:00:13 +0800

    add for textView

--- app/src/main/java/error/licen/com/erroredittext/MainActivity.java
+++ app/src/main/java/error/licen/com/erroredittext/MainActivity.java
@@ -6,11 +6,13 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextInputLayout myTextInputLayout;
+    private TextInputLayout myTextInputLayoutForTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +20,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         myTextInputLayout = (TextInputLayout) findViewById(R.id.myTextInputLayout);
+        myTextInputLayoutForTextView = (TextInputLayout) findViewById(R.id.myTextInputLayout_forTextView);
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
+
+        findViewById(R.id.myTextView).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        String message = getEditTextString();
-        if(TextUtils.isEmpty(message)){
-            myTextInputLayout.showMessage("Please enter your name");
+        String text = ((TextView)findViewById(R.id.myTextView)).getText().toString();
+
+        if(v.getId() == R.id.myTextView){
+            if(TextUtils.isEmpty(text)){
+                ((TextView)findViewById(R.id.myTextView)).setText("Hello Hello Hello");
+            }else{
+                ((TextView)findViewById(R.id.myTextView)).setText("");
+            }
             return;
         }
 
-        Toast.makeText(this, "Hi !" + message, Toast.LENGTH_SHORT).show();
+        // editText
+        String message = getEditTextString();
+        if(TextUtils.isEmpty(message)){
+            myTextInputLayout.showMessage("Please enter your name");
+        }else{
+            Toast.makeText(this, "Hi !" + message, Toast.LENGTH_SHORT).show();
+        }
+
+        //TextView
+        if(TextUtils.isEmpty(text)){
+            myTextInputLayoutForTextView.showMessage("Please enter in TextView");
+        }
     }
 
     private String getEditTextString(){
--- app/src/main/java/error/licen/com/erroredittext/TextInputLayout.java
+++ app/src/main/java/error/licen/com/erroredittext/TextInputLayout.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,8 @@ public class TextInputLayout extends LinearLayout implements TextWatcher {
 
     private FrameLayout mInputFrame;
     private TextView messageTextView;
+
+    private String TEXTVIEW_DEFAULT_STRING;
 
     public TextInputLayout(Context context) {
         super(context);
@@ -36,41 +39,58 @@ public class TextInputLayout extends LinearLayout implements TextWatcher {
         initContentView();
     }
 
-    private void initContentView(){
+    private void initContentView() {
         View root = LayoutInflater.from(getContext()).inflate(R.layout.text_input_layout, this);
         mInputFrame = (FrameLayout) root.findViewById(R.id.textInputLayout_contentLayout);
         messageTextView = (TextView) root.findViewById(R.id.textInputLayout_errorTextView);
+    }
+
+    public void setTextViewDefaultString(String text) {
+        this.TEXTVIEW_DEFAULT_STRING = text;
     }
 
     @Override
     public void addView(View child, int index, final ViewGroup.LayoutParams params) {
         if (child instanceof EditText) {
 
-            FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(params);
-            flp.gravity = Gravity.CENTER_VERTICAL | (flp.gravity & ~Gravity.VERTICAL_GRAVITY_MASK);
-            mInputFrame.addView(child, flp);
-            mInputFrame.setLayoutParams(params);
-
+            addChildViewToFrameLayout(child, params);
             setEditText((EditText) child);
+
+        } else if (child instanceof TextView) {
+
+            addChildViewToFrameLayout(child, params);
+            setTextView((TextView) child);
+
         } else {
             super.addView(child, index, params);
         }
+    }
+
+    private void addChildViewToFrameLayout(View child, ViewGroup.LayoutParams params) {
+        FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(params);
+        flp.gravity = Gravity.CENTER_VERTICAL | (flp.gravity & ~Gravity.VERTICAL_GRAVITY_MASK);
+        mInputFrame.addView(child, flp);
+        mInputFrame.setLayoutParams(params);
+    }
+
+    private void setTextView(TextView textView) {
+        textView.addTextChangedListener(this);
     }
 
     private void setEditText(EditText editText) {
         editText.addTextChangedListener(this);
     }
 
-    public void showMessage(String message){
-        if(messageTextView == null){
+    public void showMessage(String message) {
+        if (messageTextView == null) {
             return;
         }
         messageTextView.setVisibility(VISIBLE);
         messageTextView.setText(message);
     }
 
-    public void hideMessage(){
-        if(messageTextView != null){
+    public void hideMessage() {
+        if (messageTextView != null) {
             messageTextView.setVisibility(GONE);
             messageTextView.setText("");
         }
@@ -83,6 +103,7 @@ public class TextInputLayout extends LinearLayout implements TextWatcher {
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
+        Log.e("", "onTextChanged s = " + s);
         hideMessage();
     }
 
